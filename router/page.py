afeb967e6d0b8acdaccd6b7479cbee3d6d19c907
noyemkaren-ship-it <noyemkaren@gmail.com>
2026-08-29 03:47:17 +0300

    New commit

--- router/page.py
+++ router/page.py
@@ -4,37 +4,35 @@ from pathlib import Path
 
 router = APIRouter(tags=["pages"])
 
-# Путь к templates
 TEMPLATES_DIR = Path("templates")
 
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    """Главная страница"""
-    html_file = TEMPLATES_DIR / "index.html"
-    return FileResponse(html_file)
+    return FileResponse(TEMPLATES_DIR / "index.html")
 
 
 @router.get("/books_page", response_class=HTMLResponse)
 async def books_page(request: Request):
-    """Страница библиотеки"""
-    html_file = TEMPLATES_DIR / "books.html"
-    return FileResponse(html_file)
+    return FileResponse(TEMPLATES_DIR / "books.html")
 
 
 @router.get("/files", response_class=HTMLResponse)
 async def files_page(request: Request):
-    """Страница файлов"""
-    html_file = TEMPLATES_DIR / "files.html"
-    return FileResponse(html_file)
+    return FileResponse(TEMPLATES_DIR / "files.html")
 
-@router.get("/desk", response_class=HTMLResponse)
-async def get_desk(request: Request):
-    html_file = TEMPLATES_DIR / "desk.html"
-    return FileResponse(html_file)
 
 @router.get("/information", response_class=HTMLResponse)
 async def information_page(request: Request):
-    """Страница информации"""
-    html_file = TEMPLATES_DIR / "information.html"
-    return FileResponse(html_file)
+    return FileResponse(TEMPLATES_DIR / "information.html")
+
+
+@router.get("/desk", response_class=HTMLResponse)
+async def desk_page(request: Request):
+    return FileResponse(TEMPLATES_DIR / "desk.html")
+
+
+@router.get("/info/{name}", response_class=HTMLResponse)
+async def info_page(request: Request, name: str):
+    """Страница конкретной информации"""
+    return FileResponse(TEMPLATES_DIR / "info.html")
